4f1e1d25f9a9586ee22970c579a7d490e9c21eaf
noorsidhu <66785975+noorsidhu@users.noreply.github.com>
2020-10-02 18:03:46 +0530

    Update and rename Palindrone.go to Palindrome.go

---
+++ Go/Mathematical/Palindrome.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() 
+	var userInput string;
+	fmt.Println("Enter a string");
+	fmt.Scanln(&userInput);
+	var i int;
+	var userInputLength = len(userInput);
+	for i = 0; i < userInputLength ; i++ 
+		if (userInput[i] != userInput[userInputLength - 1 - i]) 
+			fmt.Println(userInput, "is not a palindrome");
+			return ;
+		
+
+	fmt.Println(userInput, "is a palindrome");
