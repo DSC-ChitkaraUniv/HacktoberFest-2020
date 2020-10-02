03dd1ddc57459b98c92439f18afe1ca34a8bf283
jagritvats <69034224+jagritvats@users.noreply.github.com>
2020-10-02 21:44:24 +0530

    Update bubbleSort.py

--- Coding/Python/bubbleSort.py
+++ Coding/Python/bubbleSort.py
@@ -4,7 +4,7 @@ a=[int(x) for x in input().split()] #line seperated input of list using list com
 
 n=len(a)
 
-for i in range(n):
+for i in range(n-1):
   for j in range(n-1-i):
     if a[j]>a[j+1]:
       a[j],a[j+1]=a[j+1],a[j]
