1a73b9069b06668b415fa2f54e4db792a2cf6b8d
jagritvats <69034224+jagritvats@users.noreply.github.com>
2020-10-03 09:39:51 +0530

    Update bubbleSort.py
    sorting done by function

--- Coding/Python/bubbleSort.py
+++ Coding/Python/bubbleSort.py
@@ -1,11 +1,26 @@
 # bubble sorting a list
 
-a=[int(x) for x in input().split()] #line seperated input of list using list comprehension
+def bubbleSort(a): # a is the list to sort
+  n=len(a) # n is the length of the list
+  for i in range(n-1):
+    for j in range(n-1-i):
+      if a[j]>a[j+1]:
+        a[j],a[j+1]=a[j+1],a[j]
+  return(a)
 
-n=len(a)
+def displayList(a):
+  for ele in a:
+    print(ele,end=" ")
+  print()
 
-for i in range(n-1):
-  for j in range(n-1-i):
-    if a[j]>a[j+1]:
-      a[j],a[j+1]=a[j+1],a[j]
-print(a)
+# lenght of list is not being taken as input becuase it is not required
+num=int(input("Enter Number of Lists to Sort:"))
+q=0
+while q<num:
+  print(q+1,"List")
+  a=[int(x) for x in input().split()] #space seperated input of list using list comprehension
+  bubbleSort(a)
+  displayList(a)
+  q+=1
+  print()
+  
