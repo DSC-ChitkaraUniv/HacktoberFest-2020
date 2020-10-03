070a104f1db865808c74ea98265426a6d6e23c28
jagritvats <69034224+jagritvats@users.noreply.github.com>
2020-10-03 09:48:52 +0530

    Update bubbleSort.py
    added some more description

--- Coding/Python/bubbleSort.py
+++ Coding/Python/bubbleSort.py
@@ -3,12 +3,12 @@
 def bubbleSort(a): # a is the list to sort
   n=len(a) # n is the length of the list
   for i in range(n-1):
-    for j in range(n-1-i):
+    for j in range(n-1-i): # after one complete pass the largest number is moved to n-1-i th index by consequtive comparisions and swaps
       if a[j]>a[j+1]:
         a[j],a[j+1]=a[j+1],a[j]
   return(a)
 
-def displayList(a):
+def displayList(a): # prints the list
   for ele in a:
     print(ele,end=" ")
   print()
@@ -16,7 +16,7 @@ def displayList(a):
 # lenght of list is not being taken as input becuase it is not required
 num=int(input("Enter Number of Lists to Sort:"))
 q=0
-while q<num:
+while q<num: 
   print(q+1,"List")
   a=[int(x) for x in input().split()] #space seperated input of list using list comprehension
   bubbleSort(a)
