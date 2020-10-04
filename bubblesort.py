e978370a91ffa00d6a42b6f10dfa3d0e350281a2
Chaitalykundu <chaitaly.slg01@gmail.com>
2020-10-04 16:16:41 +0530

    Add bubblesort using python

---
+++ bubblesort.py
@@ -0,0 +1,26 @@
+#bubblsort
+
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+  
+# Driver code to test above 
+arr = [66, 33, 55, 88, 22, 11, 99,44,77] 
+  
+bubbleSort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i])
