009049043a203cc2453fa47e8334816d8ee6af9e
Jaydeep Ghevariya <72187052+JYDP02@users.noreply.github.com>
2020-10-17 09:46:47 +0530

    Updated name of transforming variable
    There was the name of transforming variable was ct and i changed it to easy 'i'!😅😁

--- Coding/C/Sum of array.c
+++ Coding/C/Sum of array.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
-    int ct,num,sum=0;
+    int i,num,sum=0;
     scanf("%d",&num);
     int array[num];
-    for(ct=0;ct<num;ct++)
+    for(i=0;i<num;i++)
     {
-        scanf("%d ",&array[ct]);
-        sum+=array[ct];
+        scanf("%d ",&array[i]);
+        sum+=array[i];
     }
     
     printf("Sum of array is %d",sum);
